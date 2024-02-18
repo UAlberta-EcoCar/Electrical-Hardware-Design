29dbb0e0a13dbade0e7eb69cccb404483cbd55af
ddjangl <semmens@ualberta.ca>
2024-02-17 19:36:08 -0700

    last known working code

---
+++ Relay-Board_2023_Lucy_V1/Relay_Lucy_Source_v1_Cmake/build/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.15.133.1-microsoft-standard-WSL2")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.15.133.1-microsoft-standard-WSL2")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/ddjangl/EcoCar/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Source_v1_Cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
