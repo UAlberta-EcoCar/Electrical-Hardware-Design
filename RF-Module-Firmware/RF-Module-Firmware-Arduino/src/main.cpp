71fb83815d43f96630a51ce236b540aaec3f9dfd
Abinash <abinash1@ualberta.ca>
2024-03-10 14:23:42 -0600

    a

--- RF-Module-Firmware/RF-Module-Firmware-Arduino/src/main.cpp
+++ RF-Module-Firmware/RF-Module-Firmware-Arduino/src/main.cpp
@@ -1,227 +1,16 @@
+#include "hal_conf_extra.h"
 #include <Arduino.h>
 #include <LoRa.h>
 #include <SPI.h> // include libraries
+
+#include <STM32_CAN.h>
 #include <stdint.h>
-#include <stm32l4xx_hal.h>
+#include <stm32l4xx.h>
 #include <stm32l4xx_hal_can.h>
 byte msgCount = 0;     // count of outgoing messages
 int interval = 2000;   // interval between sends
 long lastSendTime = 0; // time of last packet send
 #include "packets.h"
-/* USER CODE END Includes */
-
-/* USER CODE BEGIN Private defines */
-#define STD_ID 0x101
-#define DATA_LENGTH 8
-/* USER CODE END Private defines */
-
-void MX_CAN1_Init(void);
-
-/* USER CODE BEGIN Prototypes */
-void CAN_Initialize();
-int CAN_Transmit(uint32_t _device_address, uint32_t *_buffer_pointer,
-                 int _buffer_length, uint32_t _RTR);
-
-/* USER CODE END Prototypes */
-/* USER CODE BEGIN 0 */
-CAN_TxHeaderTypeDef TxHeader;
-
-uint32_t TxMailbox;
-/* USER CODE END 0 */
-
-CAN_HandleTypeDef hcan1;
-
-/* CAN1 init function */
-void MX_CAN1_Init(void)
-{
-
-  /* USER CODE BEGIN CAN1_Init 0 */
-
-  /* USER CODE END CAN1_Init 0 */
-
-  /* USER CODE BEGIN CAN1_Init 1 */
-
-  /* USER CODE END CAN1_Init 1 */
-  hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 16;
-  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
-  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_2TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
-  hcan1.Init.TimeTriggeredMode = DISABLE;
-  hcan1.Init.AutoBusOff = DISABLE;
-  hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
-  hcan1.Init.ReceiveFifoLocked = DISABLE;
-  hcan1.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CAN1_Init 2 */
-
-  /* USER CODE END CAN1_Init 2 */
-}
-
-void HAL_CAN_MspInit(CAN_HandleTypeDef *canHandle)
-{
-
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if (canHandle->Instance == CAN1)
-  {
-    /* USER CODE BEGIN CAN1_MspInit 0 */
-
-    /* USER CODE END CAN1_MspInit 0 */
-    /* CAN1 clock enable */
-    __HAL_RCC_CAN1_CLK_ENABLE();
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**CAN1 GPIO Configuration
-    PA11     ------> CAN1_RX
-    PA12     ------> CAN1_TX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF9_CAN1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    /* CAN1 interrupt Init */
-    HAL_NVIC_SetPriority(CAN1_TX_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(CAN1_TX_IRQn);
-    HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
-    HAL_NVIC_SetPriority(CAN1_RX1_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(CAN1_RX1_IRQn);
-    /* USER CODE BEGIN CAN1_MspInit 1 */
-
-    /* USER CODE END CAN1_MspInit 1 */
-  }
-}
-
-void HAL_CAN_MspDeInit(CAN_HandleTypeDef *canHandle)
-{
-
-  if (canHandle->Instance == CAN1)
-  {
-    /* USER CODE BEGIN CAN1_MspDeInit 0 */
-
-    /* USER CODE END CAN1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_CAN1_CLK_DISABLE();
-
-    /**CAN1 GPIO Configuration
-    PA11     ------> CAN1_RX
-    PA12     ------> CAN1_TX
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11 | GPIO_PIN_12);
-
-    /* CAN1 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(CAN1_TX_IRQn);
-    HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
-    HAL_NVIC_DisableIRQ(CAN1_RX1_IRQn);
-    /* USER CODE BEGIN CAN1_MspDeInit 1 */
-
-    /* USER CODE END CAN1_MspDeInit 1 */
-  }
-}
-
-/* USER CODE BEGIN 1 */
-
-void CAN_Initialize()
-{
-  MX_CAN1_Init();
-  /* USER CODE BEGIN 2 */
-  CAN_FilterTypeDef high_priority_filter;
-  high_priority_filter.FilterIdHigh = 0x100 << 5;
-  high_priority_filter.FilterMaskIdHigh = 0x700 << 5;
-  high_priority_filter.FilterIdLow = 0x0000;
-  high_priority_filter.FilterMaskIdLow = 0x0000;
-  high_priority_filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-  high_priority_filter.FilterBank = 0;
-  high_priority_filter.FilterMode = CAN_FILTERMODE_IDMASK;
-  high_priority_filter.FilterScale = CAN_FILTERSCALE_32BIT;
-  high_priority_filter.FilterActivation = CAN_FILTER_ENABLE;
-
-  if (HAL_CAN_ConfigFilter(&hcan1, &high_priority_filter) != HAL_OK)
-  {
-    /* Filter configuration Error */
-    Error_Handler();
-  }
-
-  CAN_FilterTypeDef low_priority_filter;
-  low_priority_filter.FilterIdHigh = 0x100 << 5;
-  low_priority_filter.FilterMaskIdHigh = 0x700 << 5;
-  low_priority_filter.FilterIdLow = 0x0000;
-  low_priority_filter.FilterMaskIdLow = 0x0000;
-  low_priority_filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-  low_priority_filter.FilterBank = 0;
-  low_priority_filter.FilterMode = CAN_FILTERMODE_IDMASK;
-  low_priority_filter.FilterScale = CAN_FILTERSCALE_32BIT;
-  low_priority_filter.FilterActivation = CAN_FILTER_ENABLE;
-
-  if (HAL_CAN_Start(&hcan1) != HAL_OK)
-  {
-    printf("[!SYSTEM ERROR]CAN Initialization Error At CAN Start");
-    Error_Handler();
-  }
-
-  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
-  {
-    printf(
-        "[!SYSTEM ERROR]CAN Initialization Error At CAN INTURRUPT MESSAGE PENDING RX FIFO 0");
-    Error_Handler();
-  }
-  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO1_MSG_PENDING) != HAL_OK)
-  {
-    printf(
-        "[!SYSTEM ERROR]CAN Initialization Error At CAN INTURRUPT MESSAGE PENDING RX FIFO 1");
-    Error_Handler();
-  }
-
-  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_FULL) != HAL_OK)
-  {
-    printf(
-        "[!SYSTEM ERROR]CAN Initialization Error At CAN INTURRUPT RX FIFO 0 FULL");
-    Error_Handler();
-  }
-  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO1_FULL) != HAL_OK)
-  {
-    printf(
-        "[!SYSTEM ERROR]CAN Initialization Error At CAN INTURRUPT RX FIFO 1 FULL");
-    Error_Handler();
-  }
-
-  //	if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_ERROR))
-  //	!= HAL_OK) {
-  //		Error_Handler();
-  //	}
-  TxHeader.RTR = CAN_RTR_DATA;
-  TxHeader.IDE = CAN_ID_STD;
-  TxHeader.DLC = 8;
-  TxHeader.TransmitGlobalTime = DISABLE;
-}
-
-/**
- * Send 8 bytes at a time, with standard id size.
- */
-int CAN_Transmit(uint32_t _device_address, uint32_t *_buffer_pointer,
-                 int _buffer_length, uint32_t _RTR)
-{
-
-  TxHeader.StdId = _device_address;
-  TxHeader.RTR = _RTR;
-  if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, (uint8_t *)_buffer_pointer, &TxMailbox) != HAL_OK)
-  {
-    printf(
-        "Can transmission error on packet id: %hu and containing data: %u\r\n",
-        _device_address, _buffer_pointer);
-    Error_Handler();
-  }
-
-  return 1;
-}
 
 void sendMessage(String outgoing)
 {
@@ -250,10 +39,77 @@ void onReceive(int packetSize)
   Serial.println();
 }
 
+static CAN_message_t CAN_outMsg_1;
+static CAN_message_t CAN_outMsg_2;
+static CAN_message_t CAN_outMsg_3;
+static CAN_message_t CAN_inMsg;
+
+// This will use PA11/12 pins for CAN1 and set RX-buffer size to 64-messages. TX-buffer size is kept at default 16.
+STM32_CAN Can(CAN1, DEF, RX_SIZE_64, TX_SIZE_16);
+
+uint8_t Counter;
+
+void SendData() // Send can messages in 50Hz phase from timer interrupt.
+{
+  if (Counter >= 255)
+  {
+    Counter = 0;
+  }
+
+  // Only the counter value is updated to the 3 messages sent out.
+  CAN_outMsg_1.buf[3] = Counter;
+  Can.write(CAN_outMsg_1);
+
+  CAN_outMsg_2.buf[5] = Counter;
+  Can.write(CAN_outMsg_2);
+
+  CAN_outMsg_3.buf[6] = Counter;
+  Can.write(CAN_outMsg_3);
+
+  Serial.print("Sent: ");
+  Serial.println(Counter, HEX);
+  Counter++;
+}
+
+void readCanMessage() // Read data from CAN bus and print out the messages to serial bus. Note that only message ID's that pass filters are read.
+{
+  Serial.print("Channel:");
+  Serial.print(CAN_inMsg.bus);
+  if (CAN_inMsg.flags.extended == false)
+  {
+    Serial.print(" Standard ID:");
+  }
+  else
+  {
+    Serial.print(" Extended ID:");
+  }
+  Serial.print(CAN_inMsg.id, HEX);
+
+  Serial.print(" DLC: ");
+  Serial.print(CAN_inMsg.len);
+  if (CAN_inMsg.flags.remote == false)
+  {
+    Serial.print(" buf: ");
+    for (int i = 0; i < CAN_inMsg.len; i++)
+    {
+      Serial.print("0x");
+      Serial.print(CAN_inMsg.buf[i], HEX);
+      if (i != (CAN_inMsg.len - 1))
+        Serial.print(" ");
+    }
+    Serial.println();
+  }
+  else
+  {
+    Serial.println(" Data: REMOTE REQUEST FRAME");
+  }
+}
+
 void setup()
 {
   HAL_Init();
   SystemClock_Config();
+
   // put your setup code here, to run once:
   pinMode(PA10, OUTPUT);
   Serial.begin(115200);
@@ -272,7 +128,7 @@ void setup()
   // digitalWrite(PB5, HIGH);
   LoRa.setSPI(SPI);
 
-  if (!LoRa.begin(868E6))
+  if (!LoRa.begin(915E6))
   { // initialize ratio at 915 MHz
     Serial.println("LoRa init failed. Check your connections.");
     while (true)
@@ -284,26 +140,93 @@ void setup()
   // LoRa.setSyncWord(0xF3); // ranges from 0-0xFF, default 0x34, see API docs
   Serial.println("LoRa init succeeded.");
   // LoRa.dumpRegisters(Serial);
+
+  Counter = 0;
+
+  Can.begin();
+  Can.setBaudRate(1000000);
+  Can.enableLoopBack(true);
+  Can.setMBFilterProcessing(MB0, 0x153, 0x1FFFFFFF);
+  Can.setMBFilterProcessing(MB1, 0x613, 0x1FFFFFFF);
+  Can.setMBFilterProcessing(MB2, 0x615, 0x1FFFFFFF);
+  Can.setMBFilterProcessing(MB3, 0x1F0, 0x1FFFFFFF);
+
+  // We set the data that is static for the three different message structs once here.
+  CAN_outMsg_1.id = (0x1A5);
+  CAN_outMsg_1.len = 8;
+  CAN_outMsg_1.buf[0] = 0x03;
+  CAN_outMsg_1.buf[1] = 0x41;
+  CAN_outMsg_1.buf[2] = 0x11;
+  CAN_outMsg_1.buf[3] = 0x00;
+  CAN_outMsg_1.buf[4] = 0x00;
+  CAN_outMsg_1.buf[5] = 0x00;
+  CAN_outMsg_1.buf[6] = 0x00;
+  CAN_outMsg_1.buf[7] = 0x00;
+
+  CAN_outMsg_2.id = (0x7E8);
+  CAN_outMsg_2.len = 8;
+  CAN_outMsg_2.buf[0] = 0x03;
+  CAN_outMsg_2.buf[1] = 0x41;
+  CAN_outMsg_2.buf[3] = 0x21;
+  CAN_outMsg_2.buf[4] = 0x00;
+  CAN_outMsg_2.buf[5] = 0x00;
+  CAN_outMsg_2.buf[6] = 0x00;
+  CAN_outMsg_2.buf[7] = 0xFF;
+
+  CAN_outMsg_3.id = (0xA63);
+  CAN_outMsg_3.len = 8;
+  CAN_outMsg_3.buf[0] = 0x63;
+  CAN_outMsg_3.buf[1] = 0x49;
+  CAN_outMsg_3.buf[2] = 0x11;
+  CAN_outMsg_3.buf[3] = 0x22;
+  CAN_outMsg_3.buf[4] = 0x00;
+  CAN_outMsg_3.buf[5] = 0x00;
+  CAN_outMsg_3.buf[6] = 0x00;
+  CAN_outMsg_3.buf[7] = 0x00;
+
+// setup hardware timer to send data in 50Hz pace
+#if defined(TIM1)
+  TIM_TypeDef *Instance = TIM1;
+#else
+  TIM_TypeDef *Instance = TIM2;
+#endif
+  HardwareTimer *SendTimer = new HardwareTimer(Instance);
+  SendTimer->setOverflow(50, HERTZ_FORMAT); // 50 Hz
+#if (STM32_CORE_VERSION_MAJOR < 2)
+  SendTimer->attachInterrupt(1, SendData);
+  SendTimer->setMode(1, TIMER_OUTPUT_COMPARE);
+#else // 2.0 forward
+  SendTimer->attachInterrupt(SendData);
+#endif
+  SendTimer->resume();
 }
 
 void loop()
 {
   // Serial.println("Running Sending and Recieving");
   digitalToggle(PA10);
-  if (millis() - lastSendTime > interval)
+  // The actual code that is being used will be done to main loop as usual.
+
+  // We only read data from CAN bus if there is frames received, so that main code can do it's thing efficiently.
+  while (Can.read(CAN_inMsg))
   {
-    String message = "FormulaSucks"; // send a message
-    message += msgCount;
-    sendMessage(message);
-    Serial.println("Sending " + message);
-    lastSendTime = millis();        // timestamp the message
-    interval = random(2000) + 1000; // 2-3 seconds
-    msgCount++;
+    readCanMessage();
   }
+  // if (millis() - lastSendTime > interval)
+  // {
+  //   String message = "FormulaSucks"; // send a message
+  //   message += msgCount;
+  //   sendMessage(message);
+  //   Serial.println("Sending " + message);
+  //   lastSendTime = millis();        // timestamp the message
+  //   interval = random(2000) + 1000; // 2-3 seconds
+  //   msgCount++;
+  // }
 
   // parse for a packet, and call onReceive with the result:
   // onReceive(LoRa.parsePacket());
   // put your main code here, to run repeatedly:
+  delay(500);
 }
 
 #ifdef __cplusplus
