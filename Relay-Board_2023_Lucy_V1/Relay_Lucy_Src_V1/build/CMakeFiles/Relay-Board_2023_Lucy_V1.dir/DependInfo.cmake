3b9a07ae37a4fe1bf7f3c60158030a9d440468cd
ddjangl <97638291+ddjangl@users.noreply.github.com>
2025-01-09 10:09:03 -0700

    update

---
+++ Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/DependInfo.cmake
@@ -0,0 +1,115 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/CubeMX/startup_stm32f446xx.s" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/CubeMX/startup_stm32f446xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../USB_DEVICE/App"
+  "../USB_DEVICE/Target"
+  "../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "../"
+  "../home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/PROJECT"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/freertos.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/freertos.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/main.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/main.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/stm32f4xx_hal_msp.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/stm32f4xx_hal_timebase_tim.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/stm32f4xx_it.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/stm32f4xx_it.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/syscalls.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/syscalls.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Core/Src/system_stm32f4xx.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Core/Src/system_stm32f4xx.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/USB_DEVICE/App/usb_device.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/USB_DEVICE/App/usb_device.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/USB_DEVICE/App/usbd_cdc_if.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/USB_DEVICE/App/usbd_cdc_if.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/USB_DEVICE/App/usbd_desc.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/USB_DEVICE/App/usbd_desc.c.obj"
+  "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/USB_DEVICE/Target/usbd_conf.c" "/home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/build/CMakeFiles/Relay-Board_2023_Lucy_V1.dir/USB_DEVICE/Target/usbd_conf.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../USB_DEVICE/App"
+  "../USB_DEVICE/Target"
+  "../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "../"
+  "../home/nick_s/Electrical-Hardware-Design/Relay_Board/Relay-Board_2023_Lucy_V1/Relay_Lucy_Src_V1/PROJECT"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
